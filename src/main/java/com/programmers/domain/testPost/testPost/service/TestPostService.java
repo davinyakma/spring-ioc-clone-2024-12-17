258b954ff342f8ad48c1e0fb2ec315153ca52c65
davinyakma <poohcdv3652@naver.com>
2024-12-18 17:50:17 +0900

    feat: testPostService has testPostRepository, g

--- src/main/java/com/programmers/domain/testPost/testPost/service/TestPostService.java
+++ src/main/java/com/programmers/domain/testPost/testPost/service/TestPostService.java
@@ -1,7 +1,11 @@
 package com.programmers.domain.testPost.testPost.service;
 
+import com.programmers.domain.testPost.testPost.repository.TestPostRepository;
 import com.programmers.framework.ioc.annotations.Service;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class TestPostService {
+    private final TestPostRepository testPostRepository;
 }
